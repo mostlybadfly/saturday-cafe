94810f2da31eab302dbd35355566bea35664909a
Daniel Nunez <reachdanielnunez@gmail.com>
2017-06-14 11:39:17 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-today =  new Date();
-if (today.getDay() != 6)
-  today.setDate(today.getDate() + 7 - today.getDay());
-  
-y = today.getFullYear();
-m = today.getMonth() + 1;
-d = today.getDate();
-document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
